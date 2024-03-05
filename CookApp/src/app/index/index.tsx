07ec13b6a4fa8c369324175d3e8977729243214a
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-03-05 15:39:35 -0300

    Estrutrua do projeto criada com sucesso, teste de conexão realizado

---
+++ CookApp/src/app/index/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+
+
+
+export default function Index() {
+    return (
+        <View>
+            <Text>
+                Iniciando Com React Native Na Prática
+                Danilo Duarte
+            </Text>
+        </View>
+    )
+}
