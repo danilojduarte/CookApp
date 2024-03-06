d8600fd3addc86f20d1dfd0b99efc05a2e9419e8
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-03-06 15:00:49 -0300

    Atribuindo novos componentes e estilos

--- CookApp/src/components/ingredientes/index.tsx
+++ CookApp/src/components/ingredientes/index.tsx
@@ -1,10 +1,13 @@
 import { Pressable, Text, Image } from "react-native";
+import { styles } from "./styles";
 
 export function Ingredientes(){
     return(
-        <Pressable>
-            <Image />
-            <Text>Maçã</Text>
+        <Pressable style={styles.container}>
+            <Image source={require("@/app/assets/tomato.png")} 
+             style={styles.image}/>
+
+            <Text style={styles.title}>Maçã</Text>
         </Pressable>
     )
 }
